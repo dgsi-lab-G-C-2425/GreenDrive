acd47162dcd3cd35e8c3874d3ff39b04ebebc14e
lopezmIsmael <ismael.lopez6@alu.uclm.es>
2025-05-01 14:26:08 +0200

    mapa inicial

--- Dominio/map.py
+++ Dominio/map.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template
 from Persistencia.DAOS.UserDAO import UserDAO
 from Persistencia.AgenteBD import MongoDBAgent
+import os
 
 mongo_agent = MongoDBAgent()
 map_bp = Blueprint('map', __name__)
 
 @map_bp.route('/')
 def index():
-    return render_template('map.html')
+    google_api_key = os.getenv("GOOGLE_API_KEY")  # Leer la API key desde .env
+    return render_template('map.html', google_api_key=google_api_key)
